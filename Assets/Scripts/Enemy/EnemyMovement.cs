c215adb7de6a8cdd2691f758b1f84f502a3601e1
alfi sahri <alfisahri33@gmail.com>
2021-10-01 09:41:24 +0700

    set up enemy

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,31 +3,34 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
-    UnityEngine.AI.NavMeshAgent nav;
+  Transform player;
+  PlayerHealth playerHealth;
+  EnemyHealth enemyHealth;
+  UnityEngine.AI.NavMeshAgent nav;
 
 
-    private void Awake ()
+  private void Awake()
+  {
+    //Cari game object dengan tag player   
+    player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    //Mendapatkan Reference component
+    playerHealth = player.GetComponent<PlayerHealth>();
+    enemyHealth = GetComponent<EnemyHealth>();
+    nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+  }
+
+
+  void Update()
+  {
+    //Memindahkan posisi player
+    if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
-
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+      nav.SetDestination(player.position);
     }
-
-
-    void Update ()
+    else  //Hentikan moving
     {
-        //if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
-            nav.SetDestination (player.position);
-        //}
-        //else
-        //{
-        //    nav.enabled = false;
-        //}
+      nav.enabled = false;
     }
+  }
 }
